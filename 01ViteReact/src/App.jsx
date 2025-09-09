18c64b1ec66ebf4571f25e3fe197da2d1e49c3d3
Sudarshan <narusudarshanreddy09@gmail.com>
2025-09-10 01:27:52 +0530

    evaluated expression

--- 01ViteReact/src/App.jsx
+++ 01ViteReact/src/App.jsx
@@ -1,10 +1,14 @@
 import Luffy from "./Luffy"
 
 function App() {
+
+  const username = "Sudarshan"
   return (
     // can return only one element
+    // {} is user for evaluated expression -> cannot do operations and all , only final evaluated expression
+    // because </> is basically a object and we dont do operation in object
     <>
-      <h1>One Piece</h1>
+      <h1>{username}'s favourite One Piece Character</h1>
       <Luffy/>
     </>
   )
